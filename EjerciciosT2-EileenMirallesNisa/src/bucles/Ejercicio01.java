a58f52e8687e29bc7a7ea2fe43605a598f19c26f
DragonaEileen <eileen.miralles@iesnervion.es>
2023-11-03 14:44:21 +0100

    Ejercicio 1 Bucles Terminado
    Comenzando a añadir Try Catches

--- EjerciciosT2-EileenMirallesNisa/src/bucles/Ejercicio01.java
+++ EjerciciosT2-EileenMirallesNisa/src/bucles/Ejercicio01.java
@@ -1,5 +1,7 @@
 package bucles;
 
+import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Ejercicio01 {
@@ -16,9 +18,13 @@ public class Ejercicio01 {
 	
 	/* Pruebas */
 	/* Comienzo Pruebas -->
-	 * Entrada: 0 		| Salida Esperada: 0 		| Salida Obtenida: -0.0
-	 * Entrada: -5 		| Salida Esperada: 5.0		| Salida Obtenida: 5.0 
-	 * Entrada: 53.34 	| Salida Esperada: 53.34	| Salida Obtenida: 53.34 
+	 * Entrada: 23, 59, 59, 1	| Salida Esperada: 0, 0, 0	| Salida Obtenida: 0, 0, 0
+	 * Entrada: 0, 0, 30, 5 	| Salida Esperada: 0, 0, 35	| Salida Obtenida: 0, 0, 0
+	 * 		Error: Faltaba comprobar que los numeros llegaban a su limite para 
+	 * 			cambiarlos a 0
+	 * Entrada: 0, 0, 30, 5 	| Salida Esperada: 0, 0, 35	| Salida Obtenida: 0, 0, 35
+	 * Entrada: 0, 0, 0, 3600 	| Salida Esperada: 1, 0, 0	| Salida Obtenida: 1, 0, 0
+	 * Entrada: 0, 0, 0, 86401	| Salida Esperada: 0, 0, 1	| Salida Obtenida: 0, 0, 1
 	 * Fin Pruebas
 	 */
 
@@ -26,9 +32,9 @@ public class Ejercicio01 {
 
 		/* Declaración de Variables */
 		/* Declaramos las cuatro variables que se nos piden en el enunciado */
-		byte horas;
-		byte minutos;
-		byte segundos;
+		byte horas = -1;
+		byte minutos = -1;
+		byte segundos = -1;
 		int added;
 		
 		/* Apertura de Scanner */
@@ -36,21 +42,95 @@ public class Ejercicio01 {
 		
 		/* Syso de Información y Recogida de Datos */
 		System.out.println("Introduce la hora,y los segundos a añadir.");
+		
+		/* Usamos Bucles Do-While para que se introduzcan valores válidos */
 		//Horas
-		System.out.print("Horas: ");
-		horas = sc.nextByte();
+		do {
+			
+			System.out.print("Horas: ");
+			horas = sc.nextByte();
+			
+		}while(horas < 0 || horas >= 24);
+		
 		//Minutos
-		System.out.print("Minutos: ");
-		minutos = sc.nextByte();
+		do {
+			
+			System.out.print("Minutos: ");
+			minutos = sc.nextByte();
+			
+		}while(minutos < 0 || minutos >= 60);
+			
 		//Segundos
-		System.out.println("Segundos: ");
-		segundos = sc.nextByte();
+		do {
+			
+			try {
+				
+				System.out.print("Segundos: ");
+				segundos = sc.nextByte();
+				
+			} catch (InputMismatchException e) {
+				
+				System.err.println("El dato Introducido no tiene sentido");
+				sc.next();
+				
+			}//Fin TRY-CATCH	
+			
+		}while(segundos < 0 || segundos >= 60);
+			
 		//Segundos a Añadir
 		System.out.println("Segundos a añadir: ");
 		added = sc.nextInt();
 		
 		/* Algoritmo */
+		/* Mediante un bucle vamos a ir sumando los segundos, hasta que tengamos que 
+		 * cambiar los minutos o las horas. */
+		while (added > 0) {	//Adding Proccess
+			
+			while (horas < 24 && added > 0) {	//Horas
+			
+				while (minutos <= 59 && added > 0) {	//Minutos
+				
+					while(segundos <= 59 && added > 0) {	//Segundos
+						
+						segundos = (byte) (segundos + 1);
+						
+						added = added - 1;
+						
+					}//Fin WHILE --> Segundos
+					
+					if (segundos == 60) {
+						
+						segundos = 0;
+						
+						minutos = (byte) (minutos +1);
+						
+					}//Fin IF --> 60 segundos
+
+				}//Fin WHILE --> Minutos
+				
+				if (minutos == 60) {
+				
+					minutos = 0;
+					
+					horas = (byte) (horas + 1);
+					
+				}//Fin IF --> 60 minutos
+				
+			}//Fin WHILE --> Horas
+			
+			if(horas == 24) {
+				
+				horas = 0;
+				
+			}//Fin IF --> 24 Horas
+			
+		}//Fin WHILE --> Adding Proccess
 		
+		/* Syso de Respuesta */
+		System.out.println(horas + ":" + minutos + ":" + segundos);
+		
+		/* Cierre de Scanner */
+		sc.close();
 		
 	}
 
