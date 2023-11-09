bfa0dd53403186995c0e91af4ce50fe927e1915f
DragonaEileen <eileen.miralles@iesnervion.es>
2023-11-09 14:22:37 +0100

    Empezando Ej04 Bucles

---
+++ EjerciciosT2-EileenMirallesNisa/src/bucles/Ejercicio04.java
@@ -0,0 +1,32 @@
+package bucles;
+
+public class Ejercicio04 {
+	
+	/** Para dos números dados, a y b, es posible calcular el máximo 
+	 * común divisor (el número más grande que divide a ambos) 
+	 * mediante un algoritmo ineficiente pero sencillo: desde el 
+	 * menor de a y b, ir buscando, de forma decreciente (-1), el 
+	 * primer número que divide a ambos simultáneamente. Teniendo en 
+	 * cuenta lo que se acaba de explicar, realiza un programa que 
+	 * calcule el máximo común divisor de dos números. **/
+	
+	/* Pruebas */
+	/* Comienzo Pruebas -->
+	 * Entrada: 7	| Salida Esperada: Pyramid of 7	| Salida Obtenida: Pyramid of 7	
+	 * Entrada: hola| Salida Esperada: Exception	| Salida Obtenida: Exception
+	 * Entrada: -3	| Salida Esperada: Reintroduce	| Salida Obtenida: Reintroduce
+	 * Fin Pruebas
+	 */
+
+	public static void main(String[] args) {
+
+		/* Declaración de Variables */
+		/* Declaramos los dos números a introducir */
+		int inputA;
+		int inputB;
+		
+		/*  */
+		
+	}
+
+}
