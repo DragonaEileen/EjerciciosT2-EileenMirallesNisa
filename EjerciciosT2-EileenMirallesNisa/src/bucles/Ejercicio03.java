fa278f6868292c2a5bbf118117174fc2e69eed37
DragonaEileen <eileen.miralles@iesnervion.es>
2023-11-09 13:06:18 +0100

    Finished Exercise Loops 3

--- EjerciciosT2-EileenMirallesNisa/src/bucles/Ejercicio03.java
+++ EjerciciosT2-EileenMirallesNisa/src/bucles/Ejercicio03.java
@@ -1,5 +1,6 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio03 {
@@ -14,7 +15,7 @@ public class Ejercicio03 {
 
 	/* Pruebas */
 	/* Comienzo Pruebas -->
-	 * Entrada: 7	| Salida Esperada: 2, 3, 5, 7	| Salida Obtenida: 2, 3, 5, 7	
+	 * Entrada: 7	| Salida Esperada: Pyramid of 7	| Salida Obtenida: Pyramid of 7	
 	 * Entrada: hola| Salida Esperada: Exception	| Salida Obtenida: Exception
 	 * Entrada: -3	| Salida Esperada: Reintroduce	| Salida Obtenida: Reintroduce
 	 * Fin Pruebas
@@ -24,7 +25,7 @@ public class Ejercicio03 {
 
 		/* Declaracion de Variables */
 		/* Declaramos la variable n que se va a introducir */
-		int inputN;
+		byte inputN = 0;
 		
 		/* Apertura de Scanner */
 		Scanner sc = new Scanner(System.in);
@@ -33,12 +34,53 @@ public class Ejercicio03 {
 		System.out.println("Vamos a piramidarizar esto un poco.");
 		
 		//Altura N
-		System.out.print("Introduce la altura de la piramide: ");
-		inputN = sc.nextInt();
+		do {
+			
+			try {
+				
+				System.out.print("Introduce la altura de la piramide: ");
+				inputN = sc.nextByte();
+		
+			}catch(InputMismatchException e) {
+				
+				System.err.println("Dato introducido fuera de los límites.");
+				sc.next();
+				
+			}//Fin Try-Catch
+		
+		}while(inputN < 1 || inputN > 127 );
 		
 		/* Algoritmo */
-		/* Mediante bucles FOR, uno interno para las columnas y otro externo para las filas */
+		/* Mediante bucles FOR, uno interno para las columnas 
+		 * y otro externo para las filas */
 		
+		/* Bucle FOR de las lineas, el numero de lineas es igual al inputN
+		 * así que contamos hasta N*/
+		for(byte i = 0; i < inputN; i++) {
+			
+			/* La altura n va a ser igual al numero de * de la base 
+			 * Luego los superiores tendrían que hacer espacios primero
+			 * igual a N menos el index de la linea (desde 0)*/
+			
+			/* Printeamos los espacios necesarios, N menos el index 
+			 * de linea espacios*/
+			for (int k = 0; k <= inputN-i; k++) {
+				
+				System.out.print(" ");
+				
+			}//Fin FOR --> Espacios
+			
+			/* Printeamos los asterisco-espacios, igual al index de linea */
+			for(byte j = 0; j <= i; j++) {
+				
+				System.out.print("* ");
+				
+			}//Fin FOR --> Pyramid Line Length
+			
+			/* Salto de Linea */
+			System.out.println("");
+			
+		}//Fin FOR --> Pyramid Height N
 		
 		/* Cierre de Scanner */
 		sc.close();
